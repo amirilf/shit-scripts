5b03270c92c2d7b556255b5542f8e156fea22cc0
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-03-02 11:33:29 +0330

    add functions to make readMessage  more simple

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,5 @@
 import random
+from os import system
 
 # GLOBAL
 LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -49,6 +50,63 @@ def convertBase(number: int, firstBase: int, secondBase: int) -> int:
     return int(result[::-1])
 
 
+def multipleBaseConvertor(number: int, bases: list[int], default=True) -> int:
+    """
+    - Gets the number and converts number to bases one by one according to the list
+
+    - default:
+        - True: we already know number is based on 10
+        - False: number is based on the first item of bases
+
+    - Two consecutive bases should not be the same
+    """
+
+    if default:
+        last = 10
+        for base in bases:
+            number = convertBase(number, last, base)
+            last = base
+    else:
+        last = bases[0]
+        for base in bases[1::]:
+            number = convertBase(number, last, base)
+            last = base
+
+    return number
+
+
+def doMathematicalOperations(number: int, operations: list) -> int:
+    """
+    Takes a number and applies a set of simple mathematical operations to the number in order and returns the result.
+
+    operations should be like => [ value , operator ]
+
+    - e.g. number is 10
+        - [ [10,'*'] ]          ==> will return 100
+        - [ [12,'+'] ]          ==> will return 22
+        - [ [5,'+'] , [3,'*'] ] ==> will return 45
+    """
+
+    for operation in operations:
+
+        value = operation[0]
+        operator = operation[1]
+
+        match operator:
+            case "+":
+                number += value
+            case "-":
+                number -= value
+            case "*":
+                number *= value
+            case "/":
+                number /= value
+            case _:
+                raise Exception("Wrong operator detected, only (+,-,*,/) are accepted")
+
+    return number
+
+
 def randomLetter() -> str:
     """
     returns a random english letter
@@ -71,16 +129,16 @@ def keyGenerator() -> list:
     This function creates a key for the message
 
     key pattern => [
-        1 -> range of seprators
-        2 -> main seprator letter
-        3 -> number of bases which we use in convertBase
-        4 -> all the bases
-        5 -> 4 numbers which we use for operations (* and +)
+        - 1) range of seprators
+        - 2) main seprator letter
+        - 3) number of bases which we use in convertBase
+        - 4) all the bases
+        - 5) 3 numbers which we use for operations (+ and * and -)
 
         -> and there are lots of useless chars in between
     ]
 
-    this key will be converted into two parts with specified sizes and added to the beginning and end of the final message
+    this key will be converted into parts with specified sizes and added to the beginning and end of the final message
     parts are defined in `firstPartOfKey` and `secondPartOfKey`
     """
 
@@ -102,8 +160,8 @@ def keyGenerator() -> list:
     # add the main seprator letter
     # main seprator is the key letter which will seprate letters in complex result
     # and we can detect and seprate letters in last result by this letter and its neighbors (which are in range)
-    mainSepratorLetter = ord(LETTERS[random.randint(letterRange, 51 - letterRange)])
-    key += str(mainSepratorLetter)
+    mainSepratorLetterIndex = random.randint(letterRange, 51 - letterRange)
+    key += str(mainSepratorLetterIndex)
 
     # just a random letter in between and also it will be used to find the right ascii code of main letter
     key += randomLetter()
@@ -113,7 +171,7 @@ def keyGenerator() -> list:
     key += str(bases)
 
     # to check that there are no matching bases
-    lis = []
+    baseList = []
 
     for i in range(bases):
 
@@ -121,9 +179,9 @@ def keyGenerator() -> list:
         randBase = random.randint(5, 14)
 
         # check the absence of duplicates
-        while randBase in lis:
+        while randBase in baseList or randBase == 10:
             randBase = random.randint(5, 14)
-        lis.append(randBase)
+        baseList.append(randBase)
 
         key += str(randBase)
 
@@ -131,47 +189,78 @@ def keyGenerator() -> list:
         if doIt():
             key += randomLetter()
 
-    # these are operators which we use to make numbers more strange
-    # pattern for these 9 digits => [2 digit (*) | 3 digit (+) | 1 digit (*) | 3 digit (+)]
-    key += str(random.randint(10, 19))
+    # these are values which we use to make numbers more strange
+    # pattern for these 7 digits => [3 digit (+) | 1 digit (*) | 3 digit (-)]
+    values = []
 
-    # just a random letter to make it wierd
-    if doIt():
-        key += randomLetter()
+    values.append(random.randint(101, 299))
+    values.append(random.randint(3, 9))
+    values.append(random.randint(300, 499))
 
-    key += str(random.randint(201, 399))
+    for i in values:
+        key += str(i)
 
-    # just a random letter to make it wierd
-    if doIt():
-        key += randomLetter()
-
-    key += str(random.randint(3, 6))
-
-    # just a random letter to make it wierd
-    if doIt():
-        key += randomLetter()
-
-    key += str(random.randint(151, 199))
-
-    # just a random letter to make it wierd
-    if doIt():
-        key += randomLetter()
+        # just a random letter to make it wierd
+        if doIt():
+            key += randomLetter()
 
     keyLength = len(key)
     half = keyLength // 2
     firstPartOfKey = random.randint(half - 4, half + 4)
     secondPartOfKey = keyLength - firstPartOfKey
 
-    return [firstPartOfKey, secondPartOfKey, key]
+    return [
+        key,
+        firstPartOfKey,
+        secondPartOfKey,
+        baseList,
+        mainSepratorLetterIndex,
+        letterRange,
+        values,
+    ]
 
 
-def createMessage():
+def createMessage(text: str) -> str:
+    """
+    create the final message
+    get the key and then create the rest of it
+    """
+
+    # [key, firstPartOfKey, secondPartOfKey, baseList, mainSepratorLetter, letterRange, values]
     keyList = keyGenerator()
+
+    result = ""
+
+    for char in text:
+
+        # get the ascii number of the letter
+        charNumber = ord(char)
+
+        # convertBase job
+        charNumber = multipleBaseConvertor(charNumber, keyList[3])
+
+        # math job
+        charNumber = doMathematicalOperations(
+            charNumber,
+            [[keyList[6][0], "+"], [keyList[6][1], "*"], [keyList[6][2], "-"]],
+        )
+
+        # create rendom seprator char (which is in range)
+        seprator = LETTERS[
+            random.randint(keyList[4] - keyList[5], keyList[4] + keyList[5])
+        ]
+
+        # add the final result
+        print(seprator)
+        result += str(charNumber) + seprator
+
     print(keyList)
+    print(result)
 
 
 def readMessage():
     pass
 
 
-createMessage()
+system("cls || clear")
+createMessage("b")
