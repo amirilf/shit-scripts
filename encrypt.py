9993b05d549124abe494ac19459f65bd29173fe3
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-03-02 22:17:57 +0330

    complete readMessage & fix some issues

--- encrypt.py
+++ encrypt.py
@@ -1,8 +1,22 @@
 import random
 from os import system
+from string import ascii_letters, digits
 
 # GLOBAL
 LETTERS = "iHpqrdeOMNVWyoAbRghZzuSTcGDBkEaCjfvwYJPQUlmnIxLKXstF"
+BLABLA = ["(", "]", ".", ">", ",", "#", "!", ":", "@", "$", "&", "*", "^"]
+
+
+def isLetter(char: str) -> bool:
+    """
+    Assume we only have ascii letters and digits
+    this function returns True if `char` is a letter
+    and False if it's a digit
+    """
+
+    if 48 <= ord(char) and ord(char) <= 57:
+        return False
+    return True
 
 
 def convertBase(number: int, firstBase: int, secondBase: int) -> int:
@@ -11,6 +25,7 @@ def convertBase(number: int, firstBase: int, secondBase: int) -> int:
     convert it to `secondBase`
 
     Only positive numbers are considered (:
+    All the bases should be <= 10
 
     e.g.
         - 32 in base 4 to base 3 => 112
@@ -20,6 +35,7 @@ def convertBase(number: int, firstBase: int, secondBase: int) -> int:
     assert firstBase != secondBase, "Kidding? Bases are the same..."
     assert number >= 0, "Only a positive number!"
     assert firstBase >= 2 and secondBase >= 2, "Bases should be at least 2"
+    assert firstBase <= 10 and secondBase <= 10, "Bases should be at most 10"
 
     # firstBase to base 10
     if firstBase != 10:
@@ -50,31 +66,6 @@ def convertBase(number: int, firstBase: int, secondBase: int) -> int:
     return int(result[::-1])
 
 
-def multipleBaseConvertor(number: int, bases: list[int], default=True) -> int:
-    """
-    - Gets the number and converts number to bases one by one according to the list
-
-    - default:
-        - True: we already know number is based on 10
-        - False: number is based on the first item of bases
-
-    - Two consecutive bases should not be the same
-    """
-
-    if default:
-        last = 10
-        for base in bases:
-            number = convertBase(number, last, base)
-            last = base
-    else:
-        last = bases[0]
-        for base in bases[1::]:
-            number = convertBase(number, last, base)
-            last = base
-
-    return number
-
-
 def doMathematicalOperations(number: int, operations: list) -> int:
     """
     Takes a number and applies a set of simple mathematical operations to the number in order and returns the result.
@@ -100,7 +91,7 @@ def doMathematicalOperations(number: int, operations: list) -> int:
             case "*":
                 number *= value
             case "/":
-                number /= value
+                number //= value
             case _:
                 raise Exception("Wrong operator detected, only (+,-,*,/) are accepted")
 
@@ -115,7 +106,7 @@ def randomLetter() -> str:
     return random.choice(LETTERS)
 
 
-def sameRemainderLetters(remainder: int, size: int) -> list[str]:
+def sameRemainderLetters(remainder: int, size: int) -> str:
     """
     Returns all letters with same index remainder to size
 
@@ -168,7 +159,7 @@ def keyGenerator() -> list:
     key += randomLetter()
 
     # number of bases we use in converBase to make a magic number instead of a simple number
-    bases = random.randint(5, 7)
+    bases = random.randint(3, 5)
     key += str(bases)
 
     # to check that there are no same bases
@@ -176,12 +167,12 @@ def keyGenerator() -> list:
 
     for i in range(bases):
 
-        # range of bases are in [5,14]
-        randBase = random.randint(5, 14)
+        # range of bases are in [5,9]
+        randBase = random.randint(5, 9)
 
         # check the absence of duplicates
-        while randBase in baseList or randBase == 10:
-            randBase = random.randint(5, 14)
+        while randBase in baseList:
+            randBase = random.randint(5, 9)
         baseList.append(randBase)
 
         key += str(randBase)
@@ -207,8 +198,8 @@ def keyGenerator() -> list:
 
     keyLength = len(key)
     half = keyLength // 2
-    firstPartOfKey = random.randint(half - 5, half + 5)
-    secondPartOfKey = keyLength - firstPartOfKey
+    firstLength = random.randint(half - 5, half + 5)
+    secondLength = keyLength - firstLength
 
     return [
         key,
@@ -216,8 +207,111 @@ def keyGenerator() -> list:
         operationValues,
         divisorSize,
         sepratorRemainder,
-        [firstPartOfKey, secondPartOfKey],
+        firstLength,
+        secondLength,
     ]
+
+
+def addRandomCharsInBody(text: str, limitedLetterList: str) -> str:
+
+    result = ""
+    choiceList = "".join([i for i in LETTERS if i not in limitedLetterList])
+
+    for char in text:
+        result += char
+
+        if random.randint(1, 5) == 1:
+            result += random.choice(choiceList)
+
+    return result
+
+
+def removeRandomCharsInBody(text: str, limitedLetterList: str) -> str:
+
+    result = ""
+    choiceList = "".join([i for i in LETTERS if i not in limitedLetterList])
+
+    for char in text:
+        if char not in choiceList:
+            result += char
+
+    return result
+
+
+def addShitChars(text: str) -> str:
+    """
+    Make message more dirtier
+    """
+
+    firstIndex = random.randint(5, 10)
+    secondIndex = random.randint(20, 25)
+
+    insertText1 = (
+        random.choice(BLABLA)
+        + "".join(random.choices((ascii_letters + digits), k=random.randint(4, 8)))
+        + random.choice(BLABLA)
+    )
+    insertText2 = (
+        random.choice(BLABLA)
+        + "".join(random.choices((ascii_letters + digits), k=random.randint(4, 8)))
+        + random.choice(BLABLA)
+    )
+
+    text = (
+        text[:firstIndex]
+        + insertText1
+        + text[firstIndex:secondIndex]
+        + insertText2
+        + text[secondIndex:]
+    )
+
+    return text
+
+
+def removeShitChars(text: str) -> str:
+    indexes = []
+    for index in range(len(text)):
+        if text[index] in BLABLA:
+            indexes.append(index)
+    return (
+        text[: indexes[0]] + text[indexes[1] + 1 : indexes[2]] + text[indexes[3] + 1 :]
+    )
+
+
+def keyBodySeprator(text: str) -> list:
+
+    fBase = int(text[0])
+    fLength = ""
+    for i in text[1:]:
+        if not isLetter(i):  # so it's a digit
+            fLength += i
+        else:
+            break
+    beginIndex = 2 + len(fLength)
+    fLength = int(fLength)
+
+    sBase = int(text[-1])
+    sLength = ""
+    for i in text[-2::-1]:
+        if not isLetter(i):  # so it's a digit
+            sLength += i
+        else:
+            break
+    endIndex = 2 + len(sLength)
+    sLength = int(sLength[::-1])
+
+    text = text[beginIndex:-endIndex:]
+
+    fLength //= fBase
+    sLength //= sBase
+
+    fLength = convertBase(fLength, fBase, 10)
+    sLength = convertBase(sLength, sBase, 10)
+
+    key = text[:fLength] + text[-sLength:]
+    msg = text[fLength:-sLength]
+
+    return [key, msg]
 
 
 def createMessage(text: str) -> str:
@@ -234,20 +328,32 @@ def createMessage(text: str) -> str:
     operationValues = keyList[2]
     divisorSize = keyList[3]
     sepratorRemainder = keyList[4]
-    keyParts = keyList[5]
+    firstLength = keyList[5]
+    secondLength = keyList[6]
+
     seprators = sameRemainderLetters(sepratorRemainder, divisorSize)
+
     body = ""
     result = ""
 
+    # Splitting the key into two parts (with random sizes not necessarily same sizes )
+    firstPartKey = key[:firstLength]
+    secondPartKey = key[firstLength:]
+
+    index = -1
+    bases = len(baseList)
+
     for char in text:
+
+        index += 1
 
         # get the ascii number of the letter
         charNumber = ord(char)
 
-        # convertBase job
-        charNumber = multipleBaseConvertor(charNumber, baseList)
+        # convert bases
+        charNumber = convertBase(charNumber, 10, baseList[index % bases])
 
-        # math job
+        # math operations
         charNumber = doMathematicalOperations(
             charNumber,
             list(zip(operationValues, ["+", "*", "-"])),
@@ -255,8 +361,7 @@ def createMessage(text: str) -> str:
 
         body += str(charNumber) + random.choice(seprators)
 
-    firstPartKey = key[: keyParts[0]]
-    secondPartKey = key[keyParts[0] :]
+    body = addRandomCharsInBody(body, seprators)
 
     """
     In the last step, we have to insert the first and last size of the key length.
@@ -272,7 +377,7 @@ def createMessage(text: str) -> str:
     # beginning
     trickBase = random.randint(6, 9)
     result = str(trickBase)
-    result += str(convertBase(keyParts[0], 10, trickBase) * trickBase)
+    result += str(convertBase(firstLength, 10, trickBase) * trickBase)
     result += randomLetter()
 
     # main
@@ -283,19 +388,94 @@ def createMessage(text: str) -> str:
     # end
     result += randomLetter()
     trickBase = random.randint(6, 9)
-    result += str(convertBase(keyParts[1], 10, trickBase) * trickBase)
+    result += str(convertBase(secondLength, 10, trickBase) * trickBase)
     result += str(trickBase)
 
-    print(firstPartKey)
-    print(secondPartKey)
-    print(keyList)
-    print(body)
-    print(result)
+    # add some extra chars
+    result = addShitChars(result)
+
+    # final message is ready (:
+    return result
 
 
-def readMessage():
-    pass
+def readMessage(message: str) -> str:
+    message = removeShitChars(message)
+    key, body = keyBodySeprator(message)
+
+    sepratorRemainder = ""
+    index = 1
+    while True:
+        char = key[index]
+        if not isLetter(char):
+            sepratorRemainder += char
+            index += 1
+        else:
+            break
+    divisorSize = LETTERS.index(key[index])
+    sepratorRemainder = int(sepratorRemainder) % divisorSize
+
+    seprators = sameRemainderLetters(sepratorRemainder, divisorSize)
+    body = removeRandomCharsInBody(body, seprators)
+
+    index += 2
+    bases = int(key[index])
+    index += 1
+
+    baseList = []
+    checker = 0
+
+    while checker < bases:
+        try:
+            char = int(key[index])
+            if char == 1:
+                index += 1
+                baseList.append(10 + int(key[index]))
+            else:
+                baseList.append(char)
+            checker += 1
+        except:
+            pass
+        finally:
+            index += 1
+
+    operationValues = []
+
+    values = "".join([i for i in key[index:] if not isLetter(i)])
+    operationValues.append(int(values[:3]))
+    operationValues.append(int(values[3]))
+    operationValues.append(int(values[4:]))
+
+    # ===End of key decryption===
+
+    result = ""
+    letter = ""
+    index = -1
+
+    for char in body:
+        if isLetter(char):
+            index += 1
+            letterNumber = int(letter)
+            letter = ""
+
+            letterNumber = doMathematicalOperations(
+                letterNumber, list(zip(operationValues[::-1], ["+", "/", "-"]))
+            )
+
+            letterNumber = convertBase(letterNumber, baseList[index % bases], 10)
+
+            result += chr(letterNumber)
+
+        else:
+            letter += char
+
+    return result
 
 
+# ==============
 system("cls || clear")
-createMessage("abcde")
+msg = "hello guys!"
+print("message:", msg)
+msg = createMessage(msg)
+print("encrypted:", msg)
+msg = readMessage(msg)
+print("message:", msg)
